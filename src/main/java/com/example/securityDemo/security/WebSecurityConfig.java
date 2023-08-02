e91617c44438d85db61d1a6c971554c886fa7f9f
katsu <katsutoshi1156@outlook.jp>
2023-08-02 11:42:59 +0900

    securityFilterChainの修正および注釈の追加

--- src/main/java/com/example/securityDemo/security/WebSecurityConfig.java
+++ src/main/java/com/example/securityDemo/security/WebSecurityConfig.java
@@ -1,37 +1,51 @@
 package com.example.securityDemo.security;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
-import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 
+// Configurationクラスであることを明示
+// 従来は@EnableWebSecurityに含まれていたが、削除されたので記載が必要
 @Configuration
+// Spring Securityの設定を有効にしてカスタム可能にする
+@EnableWebSecurity
+// 従来:@EnableGlobalMethodSecurit
+// 導入:@EnableMethodSecurity
+@EnableMethodSecurity
 public class WebSecurityConfig {
+// Spring Security 5.3以前ではWebSecurityConfigurerAdapter
+// を継承したクラスでSecurityFilterChainを定義していたが、Beanとして定義可能に
 
-    // エンコード設定
     @Bean
-    PasswordEncoder passwordEncoder(){
+    PasswordEncoder passwordEncoder() {
         Pbkdf2PasswordEncoder passwordEncoder =
                 Pbkdf2PasswordEncoder.defaultsForSpringSecurity_v5_8();
         passwordEncoder.setEncodeHashAsBase64(true);
         return passwordEncoder;
     }
 
-    // ログイン、ログアウト画面を作成したHTMLに差し替える
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-        SimpleUrlAuthenticationSuccessHandler successHandler = new SimpleUrlAuthenticationSuccessHandler();
-        successHandler.setDefaultTargetUrl("/logout");
-
-        return http
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.formLogin(formLogin -> formLogin
+                        .loginProcessingUrl("/login")
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/logout")
+                        .failureUrl("/login?error")
+                        .permitAll())
+                .logout(logout -> logout
+                        .logoutSuccessUrl("/login"))
                 .authorizeHttpRequests(authorize -> authorize
-                    .requestMatchers("/login").permitAll()
-                    .anyRequest().authenticated())
-                .formLogin(formLogin -> formLogin.loginPage("/login").successHandler(successHandler))
-                .build();
+                        // 静的リソースへのアクセスを全て許可
+                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                        .requestMatchers("/login").permitAll()
+                        .anyRequest().authenticated()
+                );
+        return http.build();
     }
 }
